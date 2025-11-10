5c1361dce61fa0b962e4d6b8fb128f29433e0369
kmiit <kmiit@foxmail.com>
2025-11-10 20:27:21 +0800

    drop unexisting item

--- qcom/opensource/camera-kernel/drivers/cam_isp/cam_isp_context.c
+++ qcom/opensource/camera-kernel/drivers/cam_isp/cam_isp_context.c
@@ -6909,7 +6909,6 @@ static int __cam_isp_ctx_release_hw_in_top_state(struct cam_context *ctx,
 	ctx_isp->support_consumed_addr = false;
 	ctx_isp->aeb_enabled = false;
 	ctx_isp->sfe_en = false;
-	ctx_isp->bubble_recover_dis = false;
 	ctx_isp->req_info.last_bufdone_req_id = 0;
 	kfree(ctx_isp->vfe_bus_comp_grp);
 	kfree(ctx_isp->sfe_bus_comp_grp);
